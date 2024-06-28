20d5b65444c43ca27da54c0fb31f32daae9e76c9
raccless <philipp-schwarz@hotmail.com>
2024-06-29 01:36:47 +0200

    fixed more stuff and replaced some fillers
    yup what he said and i added more animations for normal text (also its centered now) updated the favicons on all pages

--- javascript/Portfolio-script.js
+++ javascript/Portfolio-script.js
@@ -7,6 +7,10 @@ window.onscroll = () => {
 
     document.querySelector(".stars").style.opacity = (500 - scrollY) / 500;
 
+    document.querySelector(".moon").style.marginTop = scrollY + "px";
+
+    document.querySelector(".moon").style.marginLeft = -scrollY * 4 + "px";
+
     document.querySelector(".light").style.opacity = scrollY / 150;
 
     document.querySelector(".Home h1").style.marginTop = scrollY + "px";
@@ -14,3 +18,19 @@ window.onscroll = () => {
     document.querySelector(".Home h1").style.opacity = (200 - scrollY) / 200;
 
 };
+
+const observer = new IntersectionObserver((entries) => {
+    entries .forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+        else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el))
+
